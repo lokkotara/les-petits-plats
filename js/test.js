ac230e588ee23e9bb8c17cb9f32402835685df1a
greg <lokkotarakun@gmail.com>
2021-12-03 23:10:23 +0100

    refactor: nettoyage des fichiers de test inutiles

--- js/test.js
+++
@@ -1,9 +0,0 @@
-import {setDataManagerSource, initDataManager} from './dataManager.js';
-
-
-async function start(){
-  setDataManagerSource("http://localhost:5501/datas/recipes.json");
-  await initDataManager();
-}
-
-start();
